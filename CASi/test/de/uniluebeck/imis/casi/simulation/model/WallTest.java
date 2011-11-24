cb7bebc64e36904f096cccf238af5f180d2194c5
Tobias Mende <tobias.mende@tobsolution.de>
2011-11-24 21:39:01 +0000

    CASi: adding wall test
    git-svn-id: https://dev.imis.uni-luebeck.de/svn/mate/trunk/CASi@1078 042ff3d9-7651-4cbe-9b35-41cc136bb496

---
+++ CASi/test/de/uniluebeck/imis/casi/simulation/model/WallTest.java
@@ -0,0 +1,83 @@
+package de.uniluebeck.imis.casi.simulation.model;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+
+import org.junit.Test;
+
+public class WallTest {
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWall() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddDoor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStartPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoordinates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsIPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetShapeRepresentation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCentralPoint() {
+		int counter=0;
+		// Testing 1000 random walls
+		for(int i = 0; i < 1000; i++) {
+			// Declaring a Random wall
+			int x = (int) Math.round(Math.random()*1000);
+			int y = (int) Math.round(Math.random()*1000);
+			Point start = new Point(x, y);
+			x = (int) Math.round(Math.random()*1000);
+			y = (int) Math.round(Math.random()*1000);
+			Point end = new Point(x, y);
+			Wall w = new Wall(start, end);
+			Point2D p = w.getCentralPoint();
+			if(p != null) {
+				counter++;
+			}
+		}
+		assertEquals("Not all calculated central points lay on the wall",1000, counter);
+	}
+
+}

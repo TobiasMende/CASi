7e078b597e2cc8dc357435e20edf6ad4388d1a98
Moritz Bürger <moritzb90@gmail.com>
2011-11-29 22:50:41 +0000

    Implemented slider in the simple GUI to change the scale factor. -CASi (MACK Simulator)
    git-svn-id: https://dev.imis.uni-luebeck.de/svn/mate/trunk/CASi@1117 042ff3d9-7651-4cbe-9b35-41cc136bb496

--- CASi/src/de/uniluebeck/imis/casi/ui/simplegui/ClockViewPanel.java
+++ CASi/src/de/uniluebeck/imis/casi/ui/simplegui/ClockViewPanel.java
@@ -11,12 +11,16 @@
  */
 package de.uniluebeck.imis.casi.ui.simplegui;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.FlowLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
+import de.uniluebeck.imis.casi.CASi;
 import de.uniluebeck.imis.casi.simulation.engine.ISimulationClockListener;
 import de.uniluebeck.imis.casi.simulation.engine.SimulationClock;
 import de.uniluebeck.imis.casi.simulation.model.SimulationTime;
@@ -24,65 +28,88 @@ import de.uniluebeck.imis.casi.simulation.model.SimulationTime;
 /**
  * This class extends JPanel and shows the simulation time in the simple GUI.
  * 
- * @author Moritz Bürger
+ * @author Moritz Buerger
  *
  */
 @SuppressWarnings("serial")
-public class ClockViewPanel extends JPanel implements ISimulationClockListener {
+public class ClockViewPanel extends JPanel implements ISimulationClockListener, ChangeListener {
 	
-	private SimulationTime time;
+	private JLabel timeLabel;
+	private JSlider slider;
 	
 	public ClockViewPanel() {
 		
 		/** Add the clock panel as listener on the simulation clock */
 		SimulationClock.getInstance().addListener(this);
 		
-		this.setPreferredSize(new Dimension(0, 20));
+		/** Set layout to FlowLayout */
+		this.setLayout(new FlowLayout());
+		
+		/** JLabel showing the simulation time */
+		this.timeLabel = new JLabel();
+		this.add(this.timeLabel);
+		
+		/** JSlider to set the speed*/
+		this.slider = new JSlider(JSlider.HORIZONTAL,
+				SimulationClock.MINIMUM_SCALE_FACTOR,
+				SimulationClock.MAXIMUM_SCALE_FACTOR,
+				-SimulationClock.DEFAULT_SCALE_FACTOR + 2100);
+		this.slider.setMajorTickSpacing(200);
+		this.slider.setMinorTickSpacing(100);
+		this.slider.setSnapToTicks(true);
+		this.slider.setPaintTicks(true);
+		this.slider.addChangeListener(this);
+		this.add(this.slider);
+		
+		this.setPreferredSize(new Dimension(0, 35));
 	}
-	
-	public void paintComponent(Graphics g) {
 
-		super.paintComponent(g);
-
-		g.setColor(Color.BLACK);
-		g.drawString(time.toString(), 15, 15);
-	}
-
-	/* (non-Javadoc)
-	 * @see de.uniluebeck.imis.casi.simulation.engine.ISimulationClockListener#timeChanged(de.uniluebeck.imis.casi.simulation.model.SimulationTime)
-	 */
 	@Override
 	public void timeChanged(SimulationTime newTime) {
 
-		this.time = newTime;
-		this.repaint();
+		this.timeLabel.setText(newTime.toString());
 		
 	}
 
-	/* (non-Javadoc)
-	 * @see de.uniluebeck.imis.casi.simulation.engine.ISimulationClockListener#simulationPaused(boolean)
-	 */
 	@Override
 	public void simulationPaused(boolean pause) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/* (non-Javadoc)
-	 * @see de.uniluebeck.imis.casi.simulation.engine.ISimulationClockListener#simulationStopped()
-	 */
 	@Override
 	public void simulationStopped() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/* (non-Javadoc)
-	 * @see de.uniluebeck.imis.casi.simulation.engine.ISimulationClockListener#simulationStarted()
-	 */
 	@Override
 	public void simulationStarted() {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+		
+		/** Calculate the new scale factor */
+		int newScaleFactor = this.slider.getValue();
+		newScaleFactor = -newScaleFactor + 2100;
+		
+		/** Only change at values that are multiple of 100 */
+		if(newScaleFactor % 100 == 0) {
+			
+			/** Only change if the value has changed */
+			if(SimulationClock.getInstance().getScaleFactor() != 
+				newScaleFactor) {
+				
+				CASi.SIM_LOG.info("Set simulation delay to: "+newScaleFactor);
+				/** Set new value to SimulationClock */
+				SimulationClock.getInstance().setScaleFactor(newScaleFactor);
+				
+			}
+		}
+		
 		
 	}
 

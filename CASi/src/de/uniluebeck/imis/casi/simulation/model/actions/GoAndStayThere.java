9f880f50dca545837994fefd4160e96175e2c9a8
Marvin Frick <marv@hostin.is>
2012-01-19 15:22:43 +0000

    CASi: new very simple GoAndStayThere action
    git-svn-id: https://dev.imis.uni-luebeck.de/svn/mate/trunk/CASi@1355 042ff3d9-7651-4cbe-9b35-41cc136bb496

---
+++ CASi/src/de/uniluebeck/imis/casi/simulation/model/actions/GoAndStayThere.java
@@ -0,0 +1,40 @@
+/*  	CASi Context Awareness Simulation Software
+ *   Copyright (C) 2012 2012  Moritz Bürger, Marvin Frick, Tobias Mende
+ *
+ *  This program is free software. It is licensed under the
+ *  GNU Lesser General Public License with one clarification.
+ *  
+ *  You should have received a copy of the 
+ *  GNU Lesser General Public License along with this program. 
+ *  See the LICENSE.txt file in this projects root folder or visit
+ *  <http://www.gnu.org/licenses/lgpl.html> for more details.
+ */
+package de.uniluebeck.imis.casi.simulation.model.actions;
+
+import de.uniluebeck.imis.casi.simulation.model.IPosition;
+import de.uniluebeck.imis.casi.simulation.model.actionHandling.ComplexAction;
+
+/**
+ * Very simple but ComplexAction that lets an Agent move to a given position and
+ * stays there for a given time.
+ * 
+ * @author Marvin Frick
+ * 
+ */
+public class GoAndStayThere extends ComplexAction {
+
+	/**
+	 * Creates this simple action that lets the Agent move to pos and stay there
+	 * for duration.
+	 * 
+	 * @param pos
+	 *            where should the agent go
+	 * @param duration
+	 *            how long should the agent stay there
+	 */
+	public GoAndStayThere(IPosition pos, int duration) {
+		addSubAction(new Move(pos));
+		addSubAction(new StayHere(duration, 5));
+	}
+
+}

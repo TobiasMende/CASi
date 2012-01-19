65c801c13a42ac96613b3864a9947a7656ac0181
Marvin Frick <marv@hostin.is>
2012-01-19 09:26:29 +0000

    CASi: mehr Actionen der Agents. textual_description.txt ist komplett umgesetzt.
    git-svn-id: https://dev.imis.uni-luebeck.de/svn/mate/trunk/CASi@1349 042ff3d9-7651-4cbe-9b35-41cc136bb496

--- CASi/src/de/uniluebeck/imis/casi/simulations/mate/generator/java/Actions.java
+++ CASi/src/de/uniluebeck/imis/casi/simulations/mate/generator/java/Actions.java
@@ -11,10 +11,12 @@
  */
 package de.uniluebeck.imis.casi.simulations.mate.generator.java;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 import de.uniluebeck.imis.casi.generator.ActionCollector;
 import de.uniluebeck.imis.casi.generator.AgentCollector;
@@ -46,21 +48,26 @@ import de.uniluebeck.imis.casi.simulation.model.mackComponents.Desktop;
  */
 public final class Actions {
 
+	private static final Logger log = Logger.getLogger(WorldGenerator.class
+			.getName());
+
 	/**
 	 * Fills the ActionGenerator singleton object with all the actions.
 	 * 
 	 * Put all your actions here!
 	 */
 	public static void generateActions(SimulationTime simulationStartTime) {
-		ActionCollector ag = ActionCollector.getInstance();
-		ComponentCollector cc = ComponentCollector.getInstance();
+		RoomCollector roomC = RoomCollector.getInstance();
+		AgentCollector agentC = AgentCollector.getInstance();
+		ActionCollector actionC = ActionCollector.getInstance();
+		ComponentCollector componentC = ComponentCollector.getInstance();
 
 		// 09:00 - Agents are working on their desktops
-		for (Agent a : AgentCollector.getInstance().getAll()) {
+		for (Agent a : agentC.getAll()) {
 			AbstractComponent ac;
 			String ident = "Desktop-" + a.getIdentifier();
 
-			ac = cc.findComponentByIdentifier(ident);
+			ac = componentC.findComponentByIdentifier(ident);
 			Desktop.Program randomProgramm = Desktop.Program.values()[(int) (Math
 					.random() * Desktop.Program.values().length)];
 			Desktop.Frequency randomFrequency = Desktop.Frequency.values()[(int) (Math
@@ -70,38 +77,83 @@ public final class Actions {
 
 			// the actionCollector key has to start with the Agents identifier!
 			// Otherwise is would not be added to his todoList automatically!
-			ag.newAction(a.getIdentifier() + "action_" + ident + "_1",
+			actionC.newAction(a.getIdentifier() + "action_" + ident + "_1",
 					workOnDesktop);
 
 			// also add this action to the agents actionPool
-			ag.newAction(a.getIdentifier() + "action_" + ident + "_pool",
+			actionC.newAction(a.getIdentifier() + "action_" + ident + "_pool",
 					workOnDesktop.clone());
 		}
 
 		// 10:00 - Felix goes to the kitchen to drink a coffee
-		Agent felix = AgentCollector.getInstance().findAgentByName(
-				"Felix Freudentanz");
-		ComplexAction newAction = new ComplexAction();
-		newAction.setDeadline(simulationStartTime.plus(60 * 60 + 10 * 60));
-		newAction.addSubAction(new Move(RoomCollector.getInstance()
+		Agent felix = agentC.findAgentByName("Felix Freudentanz");
+		ComplexAction goToDrinkACoffee = new ComplexAction();
+		goToDrinkACoffee.setDeadline(simulationStartTime
+				.plus(60 * 60 + 10 * 60));
+		goToDrinkACoffee
+				.setEarliestStartTime(simulationStartTime.plus(60 * 60));
+		goToDrinkACoffee.addSubAction(new Move(roomC
 				.findRoomByIdentifier("kitchen")));
-		newAction.addSubAction(new StayHere(5, 3));
-		newAction.addSubAction(new Move(RoomCollector.getInstance()
+		goToDrinkACoffee.addSubAction(new StayHere(5, 3));
+		goToDrinkACoffee.addSubAction(new Move(roomC
 				.findRoomByIdentifier("officeFelix")));
-		ActionCollector.getInstance().newAction(
-				felix.getIdentifier() + "_goesToTheKitchenToDrinkCoffee",
-				newAction);
+		actionC.newAction(felix.getIdentifier()
+				+ "_goesToTheKitchenToDrinkCoffee", goToDrinkACoffee);
 
 		// 10:30 - Hermann creates a meeting for 12:00
 		// Meeting: takes 2 hours, every employee attends, is in Hermanns room
-		Agent hermann = AgentCollector.getInstance().findAgentByName(
-				"Hermann Matsumbishi");
-		AbstractAction createMeeting = new CreateAMeeting(simulationStartTime.plus(60*60), hermann,
-				AgentCollector.getInstance().getAll(), RoomCollector
-						.getInstance().findRoomByIdentifier("officeHermann"),
-				simulationStartTime.plus(60*60*3), 120, 10);
-		ActionCollector.getInstance().newAction(hermann.getIdentifier()+"_createsThe12oclockMeeting", createMeeting);
-		
+		Agent hermann = agentC.findAgentByName("Hermann Matsumbishi");
+		AbstractAction createMeeting = new CreateAMeeting(
+				simulationStartTime.plus(60 * 60), hermann, AgentCollector
+						.getInstance().getAll(),
+				roomC.findRoomByIdentifier("officeHermann"),
+				simulationStartTime.plus(60 * 60 * 3), 120, 9);
+		actionC.newAction(hermann.getIdentifier()
+				+ "_createsThe12oclockMeeting", createMeeting);
+
+		// between 15:00 and 15:45: Felix visits Hermann for half an hour
+		AbstractAction goToHermann = new GoAndSpeakTo(hermann,30);
+		// using real timestamps to parse here. SimulationsStartTime is:
+		// 04/23/2012 09:00:42
+		try {
+			goToHermann.setEarliestStartTime(new SimulationTime(
+					"04/23/2012 15:00:00"));
+			goToHermann.setEarliestStartTime(new SimulationTime(
+					"04/23/2012 15:45:00"));
+		} catch (ParseException e) {
+			log.warning("Could not parse timeString for simulationClock!");
+		}
+		actionC.newAction(
+				felix.getIdentifier() + "_goesToHermannForHalfAnHour",
+				goToHermann);
+
+		// till 15:30: Dagobert has to work exactly 42 very concentrated minutes
+		// in his text
+		// application
+		Agent dagobert = agentC.findAgentByName("Dagobert Dreieck");
+		AbstractAction dagobertHasToWork = new GoAndWorkOnDesktop(
+				(Desktop) componentC.findComponentByIdentifier("Desktop-"
+						+ dagobert.getIdentifier()), Desktop.Program.text,
+				Desktop.Frequency.very_active, 42);
+		try {
+			dagobertHasToWork.setDeadline(new SimulationTime(
+					"04/23/2012 15:30:23"));
+		} catch (ParseException e) {
+			log.warning("Could not parse timeString for simulationClock!");
+		}
+		actionC.newAction(dagobert.getIdentifier() + "_hasToWork",
+				dagobertHasToWork);
+
+		// 16:00 Felix geht zu Zwotah ins Büro, sie reden über Belangloses. Das
+		// Mike hört das. Der Würfel sollte jetzt erkennen, dass die beiden
+		// beschäftigt sind. Zwotah dreht seinen WÜrfel auf "nicht beschäftigt".
+		AbstractAction felixTalksWithZwotah = new GoAndSpeakTo(
+				agentC.findAgentByName("Zwotah Zwiebel"), 60);
+		felixTalksWithZwotah.setEarliestStartTime(simulationStartTime
+				.plus(60 * 60 * 7));
+		// they just talk about the weekend, nothing important.
+		felixTalksWithZwotah.setPriority(2);
+
 	}
 
 	/**

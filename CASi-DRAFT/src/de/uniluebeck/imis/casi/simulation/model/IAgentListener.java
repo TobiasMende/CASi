9f22718d2025b23b11116b7c903476250fc60f38
Tobias Mende <tobias.mende@tobsolution.de>
2012-02-10 12:35:02 +0100

    Cleaning master branch

--- CASi-DRAFT/src/de/uniluebeck/imis/casi/simulation/model/IAgentListener.java
+++
@@ -1,6 +0,0 @@
-package de.uniluebeck.imis.casi.simulation.model;
-
-public interface IAgentListener {
-	public void stateChanged(Agent.STATE newState);
-	public void positionChanged(IPosition oldPosition, IPosition newPosition);
-}
